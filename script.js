38a92c7c7d42940cd308e2c602168ed612580b73
unknown <breshna2004@gmail.com>
2024-08-22 17:35:06 +0430

    slove linters error

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 const hamMenu = document.querySelector('.ham-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
+  hamMenu.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
 });
 
-document.querySelectorAll("#mobile-menu a").forEach((link) => {
+document.querySelectorAll('#mobile-menu a').forEach((link) => {
   link.addEventListener("click", () => {
-      document.querySelector(".mobile-menu.active").classList.add('hidden');
-      document.querySelector(".ham-menu.active span:nth-child(1)").classList.add('hidden');
-      document.querySelector(".ham-menu.active span:nth-child(3)").classList.add('hidden');
+    document.querySelector('.mobile-menu.active').classList.add('hidden');
+    document.querySelector('.ham-menu.active span:nth-child(1)').classList.add('hidden');
+    document.querySelector('.ham-menu.active span:nth-child(3)').classList.add('hidden');
   });
 });
-
-
 
 const popup1 = document.getElementById('popup1');
 const popup1Btn = document.getElementById('menuImg');
 const close1Btn = document.getElementById('close1Btn');
 
-popup1Btn.addEventListener('click', function() {
-    popup1.style.display = 'block';
+popup1Btn.addEventListener('click', () => {
+  popup1.style.display = 'block';
 });
 
-close1Btn.addEventListener('click', function() {
-    popup1.style.display = 'none';
+close1Btn.addEventListener('click', () => {
+  popup1.style.display = 'none';
 });
-
-
-
 
 const slides = document.querySelectorAll('.slide');
 const circles = document.querySelector('.circles');
@@ -37,10 +32,8 @@ slides.forEach((slide, index) => {
   const circle = document.createElement('div');
   circle.classList.add('circle');
   circle.addEventListener('click', () => {
-    setActiveSlide(index)
-    ;
-  
-  });
+    setActiveSlide(index);
+});
   circles.appendChild(circle);
 });
 
@@ -50,31 +43,20 @@ setActiveSlide(currentSlide);
 function setActiveSlide(index) {
   slides[currentSlide].classList.remove('actives');
   circles.children[currentSlide].classList.remove('actives');
-  circles.children[currentSlide].style.backgroundColor ='white';
+  circles.children[currentSlide].style.backgroundColor = 'white';
   currentSlide = index;
   slides[currentSlide].classList.add('actives');
   circles.children[currentSlide].classList.add('actives');
-  circles.children[currentSlide].style.backgroundColor ='#511F52';
+  circles.children[currentSlide].style.backgroundColor = '#511F52';
 }
-
-
-
-
-
-
-
-
-
 
 var div = document.getElementById('main1');
 var display = 1;
 
-function hideShow()
-{
-    if(display == 0)
-    {
-      div.style.display = 'none';
-      display = 1;
+function hideShow() {
+  if (display == 0) {
+    div.style.display = 'none';
+    display = 1;
     }  else {
         
         div.style.display = 'block';
